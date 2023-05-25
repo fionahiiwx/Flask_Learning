6e70590b0e92ddad28876ee2071ae0c47938a0c4
Sparklez_Foxy <fionahii@outlook.my>
2023-05-25 15:20:04 +0800

    Until 20:29

--- Hello_World/app.py
+++ Hello_World/app.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+db = SQLAlchemy() # db intitialized here
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+db.init_app(app)
 
 class Todo(db.Model):
-    id = db.Column(db.Interger, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False) # nullable=False, user can't make an empty content
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
@@ -15,6 +16,9 @@ class Todo(db.Model):
         # s string that returns the new element
         return '<Task %r>' % self.id
 
+with app.app_context():
+    db.create_all()
+    
 @app.route('/')
 
 def index():
